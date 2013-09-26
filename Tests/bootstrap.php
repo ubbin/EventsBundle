7b84f44113dced11787e036edcc9fcbd6978fe2a
trindade <trindade.pedro@gmail.com>
2013-09-26 12:18:32 +0100

    updated scrutinizer config

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,8 +1,7 @@
 <?php
 
-$file = __DIR__.'/../vendor/autoload.php';
-if (!file_exists($file)) {
-    throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"');
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
 }
 
-require_once $file;
+require $autoloadFile;
